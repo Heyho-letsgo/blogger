b7c08529950e88e26f64cd9fad384add06a48c37
Heyho-letsgo <abaudin@gmail.com>
2013-12-12 20:58:24 +0100

    second  blogger commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,22 @@ class ArticlesController < ApplicationController
 		# identifiant
 		@article = Article.find(params[:id])
 	end
+		#Cette méthode dit
+		# Affiche la page new
+	def new
+		@article = Article.new
+	end
+		#Cette méthode dit
+		# Crée et Sauve les valeurs saisies
+	def create
+		@article = Article.new
+			(
+		 	title: params[:article][:title],
+    		body: params[:article][:body]
+    		)
+  		@article.save
+  redirect_to article_path(@article)
+
+	end
+
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+	# Cette action dit que params 
+	# n'autorise que deux champs "title" et "body"
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
